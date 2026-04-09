917b3d80dd0673706552095033e40469a5f4980c
jasc1204 <185645269+jasc1204@users.noreply.github.com>
2026-04-09 11:02:49 +0000

    Added routes for email verification, confirmation, and password recovery pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,10 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Workout from './pages/Workout';
 import WorkoutHistory from './pages/WorkoutHistory';
+import VerifyEmail from './pages/VerifyEmail';
+import EmailConfirmation from './pages/EmailConfirmation';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 // test
 
@@ -44,6 +48,14 @@ function App(){
         <Route path = "/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
 
         <Route path = "/register" element={<Register />} />
+
+        <Route path="/verify-email" element={<VerifyEmail />} />
+
+        <Route path="/email-confirmation" element={<EmailConfirmation />} />
+
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route path="/reset-password" element={<ResetPassword />} />
 
         <Route path = "/profile" element={isLoggedIn ? <Profile /> : <Navigate to='/login' />} />
 
