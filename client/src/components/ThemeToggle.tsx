51911598d53c043727de77d7e2bb5958949ddd65
Linh Tran <li415173@ucf.edu>
2026-04-12 13:38:08 -0400

    Updated theme toggle to maintain light or dark mode based on user selection of mode instead of resetting to light unconditionally upon refresh

--- client/src/components/ThemeToggle.tsx
+++ client/src/components/ThemeToggle.tsx
@@ -6,14 +6,18 @@ const ThemeToggle = () => {
     // State to track whether dark mode is enabled or not.
     // isDarkMode is a boolean that is initially set to false (light mode).
     // setIsDarkMode is a function that can be used to update the isDarkMode state.
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        return localStorage.getItem("theme") === "dark";
+    });
 
     // Updates the <body> class based on the isDarkMode state.
     useEffect(() => {
         if (isDarkMode) {
             document.body.classList.add('dark');
+            localStorage.setItem("theme", "dark");
         } else {
             document.body.classList.remove('dark');
+            localStorage.setItem("theme", "light");    
         }
     }, [isDarkMode]);
     
